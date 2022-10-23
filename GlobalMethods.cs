28efdd6eb4f027371a7ed66126f277113ad415ff
IlyaGardin <113240912+IlyaGardin@users.noreply.github.com>
2022-10-23 20:16:01 +0400

    Add files via upload

---
+++ GlobalMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalMethods
+{
+    public static class GlobalMethods
+    {
+        public static int[] GetRandomArr(int minValue, int maxValue, int lengthArr)
+        {
+            Random random = new Random(); 
+
+            int[] arr = new int[lengthArr];
+
+            for (int i = 0; i < lengthArr; i++)
+            {
+                arr[i] = random.Next(minValue, maxValue);
+            }
+
+            return arr;
+        }
+        public static void PrintArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
