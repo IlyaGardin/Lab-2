acf7c04b5631d7c872b0813d78cd04c294903f08
IlyaGardin <113240912+IlyaGardin@users.noreply.github.com>
2022-10-23 20:15:20 +0400

    Add files via upload

---
+++ T9/LocalMethods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T9
+{
+    internal class LocalMethods
+    {
+        public static int[] GetNonRepeatingArr(int lengtharr)
+        {
+            int[] array = new int[lengtharr];
+            int element;            
+            for (int i = 0; i < lengtharr; i++)
+            {
+                Console.Write(i+1 + "-й элемент: ");
+                if (int.TryParse(Console.ReadLine(), out element) == false)
+                {
+                    Console.WriteLine("\nВходные данные имели не подходящий символ.");
+                    Console.WriteLine("Введите заново...\n");
+                    i--;
+                    continue;
+                }
+                array[i] = element;
+            }
+            return array;
+        }
+
+        public static bool IsSameArr(int[] arr1, int[] arr2)
+        {
+            Array.Sort(arr1);
+            Array.Sort(arr2);
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if(arr1[i] != arr2[i])
+                {
+                    return false;
+                }
+                
+            }
+            return true;
+        }
+    }
+}
---
+++ T9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T9
+{
+    internal class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число элементов массива: ");
+            int numberElem;
+            if (int.TryParse(Console.ReadLine(), out numberElem) && numberElem > 0)
+            {
+                Console.WriteLine("Заполните первый массив: ");
+                int[] arr1 = LocalMethods.GetNonRepeatingArr(numberElem);
+
+                Console.WriteLine("Заполните второй массив: ");
+                int[] arr2 = LocalMethods.GetNonRepeatingArr(numberElem);
+
+                Console.WriteLine("Массив №1:");
+                GlobalMethods.GlobalMethods.PrintArr(arr1);
+
+                Console.WriteLine("\nМассив №2:");
+                GlobalMethods.GlobalMethods.PrintArr(arr2);
+                
+                if (LocalMethods.IsSameArr(arr1,arr2) == false)
+                {
+                    Console.WriteLine("\nМассивы различны");
+                }
+                else
+                {
+                    Console.WriteLine("\nМассивы одинаковые");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректно введено число элементов массива.");
+            }
+        }
+    }
+}
