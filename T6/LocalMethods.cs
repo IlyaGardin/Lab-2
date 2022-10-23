f1bd630d831bc383dd9314c6692b4df5ee591322
IlyaGardin <113240912+IlyaGardin@users.noreply.github.com>
2022-10-23 20:13:01 +0400

    Add files via upload

---
+++ T6/LocalMethods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6
+{
+    internal class LocalMethods
+    {
+        public static int[] GetDoubledArr(int[] arr, int lengthArr)
+        {
+            int count = 0;
+            for (int i = 0; i < lengthArr; i++)
+            {                
+                if (arr[i] < 0)
+                {
+                    count++;
+                }
+            }
+            int[] doubledArr = new int[lengthArr + count];
+            
+            int countI = 0;
+            for (int i = 0; i < lengthArr; i++)
+            {
+
+                if (arr[i] > 0)
+                {
+                    doubledArr[i + countI] = arr[i];
+
+                }
+                else
+                {
+                    countI++;
+                    doubledArr[i + countI - 1] = arr[i];
+                    doubledArr[i + countI] = arr[i];
+
+                }
+
+            }
+            return doubledArr;
+        }
+    }
+}
---
+++ T6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            int[] arr = GlobalMethods.GlobalMethods.GetRandomArr(-100, 100, 10);
+           
+            Console.WriteLine("Исходный массив");
+            GlobalMethods.GlobalMethods.PrintArr(arr);
+           
+            int[] NewArr = LocalMethods.GetDoubledArr(arr, 10);
+            Console.WriteLine("Новый массив");
+            
+            GlobalMethods.GlobalMethods.PrintArr(NewArr);
+        }
+    }
+}
