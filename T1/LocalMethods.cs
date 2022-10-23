b8a74ab35bf7d607d7b9e2b1c6b2ef6fed7c8a2c
IlyaGardin <113240912+IlyaGardin@users.noreply.github.com>
2022-10-23 20:06:40 +0400

    Add files via upload

---
+++ T1/LocalMethods.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    internal class LocalMethods
+    {
+        public static void ShellSort(int[] arr)
+        {
+            int step = arr.Length / 2;
+            while (step > 0)
+            {
+                int i, j;
+                for (i = step; i < arr.Length; i++)
+                {
+                    int value = arr[i];
+                    for (j = i - step; (j >= 0) && (arr[j] > value); j -= step)
+                    {
+                        arr[j + step] = arr[j];
+                    }
+                        arr[j + step] = value;
+                    
+                }
+                step /= 2;
+            }
+
+        }
+        public static int GetMax(int[] arr)
+        {
+            int max = int.MinValue;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+
+        public static int GetMin(int[] arr)
+        {
+            int min = int.MaxValue;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            return min;
+        }
+    }
+}
---
+++ T1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace T1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] arr = GlobalMethods.GlobalMethods.GetRandomArr(0, 100, 10);
+
+            Console.WriteLine("Максимальный эллемент:{0} ", LocalMethods.GetMax(arr)) ;
+            
+            Console.WriteLine("Минимальный эллемент: {0}", LocalMethods.GetMin(arr));
+
+            LocalMethods.ShellSort(arr);
+
+            GlobalMethods.GlobalMethods.PrintArr(arr);
+            
+        }
+    }
+}
