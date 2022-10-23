004982e17cf17e93050f5733c8b3be8bfa35e4eb
IlyaGardin <113240912+IlyaGardin@users.noreply.github.com>
2022-10-23 20:13:51 +0400

    Add files via upload

---
+++ T7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GlobalMethods;
+
+namespace T7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число элементов массива: ");
+            int numberElem;
+            if (int.TryParse(Console.ReadLine(), out numberElem) && numberElem>0)
+            {
+                int[] arr = GlobalMethods.GlobalMethods.GetRandomArr(0, 100, numberElem);
+
+                Array.Sort(arr);
+                Array.Reverse(arr);
+
+                Console.WriteLine("Отсортированный массив");
+                GlobalMethods.GlobalMethods.PrintArr(arr);
+            }
+            else
+            {
+                Console.WriteLine("Некорректно введено число элементов массива.");
+            }
+        }
+    }
+}
