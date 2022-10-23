c70d7b2b5468df66612df9a27475a79982bf6e75
IlyaGardin <113240912+IlyaGardin@users.noreply.github.com>
2022-10-23 20:12:16 +0400

    Add files via upload

---
+++ T5/LocalMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5
+{
+    internal class LocalMethods
+    {
+        public static bool IsPositive(int n)
+        {
+            return n >= 0;
+        }
+    }
+}
---
+++ T5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace T5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = GlobalMethods.GlobalMethods.GetRandomArr(-100, 100, 10);
+            GlobalMethods.GlobalMethods.PrintArr(arr);
+            
+            arr = Array.FindAll(arr, LocalMethods.IsPositive).ToArray();
+                Console.WriteLine("Массив без отрицательных чисел");
+            GlobalMethods.GlobalMethods.PrintArr(arr);
+        }
+             
+        
+    }
+}
