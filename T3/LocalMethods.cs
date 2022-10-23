2b9a97e57486a67d9eb2573f6ec2dc6e86f73601
IlyaGardin <113240912+IlyaGardin@users.noreply.github.com>
2022-10-23 20:09:56 +0400

    Add files via upload

---
+++ T3/LocalMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    internal class LocalMethods
+    {
+        public static int GetSumPositiv(int[] arr)
+        {
+            int sumPositiv = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= 0)
+                {
+                    sumPositiv += arr[i];
+                }
+            }
+            return sumPositiv;
+        }
+    }
+}
---
+++ T3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = GlobalMethods.GlobalMethods.GetRandomArr(-100, 100, 20);
+
+            GlobalMethods.GlobalMethods.PrintArr(arr);           
+            
+            Console.WriteLine();
+            Console.WriteLine("Сумма неотрицательных эллементов массива:"+ LocalMethods.GetSumPositiv(arr));
+            
+        }
+    }
+}
